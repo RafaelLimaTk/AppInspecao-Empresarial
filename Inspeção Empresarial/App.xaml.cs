7e65226407b7b07e1acf49d41ed9cb5a74ae7828
RafaelLimaTk <rafamano123.rl@gmail.com>
2023-12-13 13:44:08 -0300

    Modificação no EntrySearch

--- Inspeção Empresarial/App.xaml.cs
+++ Inspeção Empresarial/App.xaml.cs
@@ -1,12 +1,52 @@
-﻿namespace Inspeção_Empresarial
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿using Microsoft.Maui.Platform;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 
-            MainPage = new AppShell();
-        }
+namespace Inspeção_Empresarial;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        CustomHandler();
+
+        MainPage = new AppShell();
+    }
+
+    private void CustomHandler()
+    {
+        EntryBorder();
+        DatePickerBorder();
+    }
+
+    private static void EntryBorder()
+    {
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("Border", (handler, view) =>
+        {
+#if ANDROID
+                //Android
+                handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+#elif IOS || MACCATALYST
+                //IOS e MAC
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+            //Windows
+            handler.PlatformView.BorderThickness = new Thickness(0).ToPlatform();
+#endif
+        });
+    }
+
+    private static void DatePickerBorder()
+    {
+        Microsoft.Maui.Handlers.DatePickerHandler.Mapper.AppendToMapping("Border", (handler, view) =>
+        {
+#if ANDROID
+                //Android
+                handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+#elif WINDOWS
+            //Windows
+            handler.PlatformView.BorderThickness = new Thickness(0).ToPlatform();
+#endif
+        });
     }
 }
--- Inspeção Empresarial/Views/Inspection/ListPage.xaml.cs
+++ Inspeção Empresarial/Views/Inspection/ListPage.xaml.cs
@@ -1,3 +1,5 @@
+using Inspeção_Empresarial.ViewModels.Inspection;
+
 namespace Inspeção_Empresarial.Views.Inspection;
 
 public partial class ListPage : ContentPage
@@ -5,5 +7,10 @@ public partial class ListPage : ContentPage
 	public ListPage()
 	{
 		InitializeComponent();
-	}
+    }
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+        Search.Focus();
+    }
 }
